e5fd47b18c0ecd2606364a32341aef2dbc2a0c34
the_mr17 <saptakmanna100@gmail.com>
2023-07-09 16:01:32 +0530

    Implementing RD

--- app/src/main/java/com/mr_17/deposit_calculator/RecurringDepositFragment.kt
+++ app/src/main/java/com/mr_17/deposit_calculator/RecurringDepositFragment.kt
@@ -3,14 +3,19 @@ package com.mr_17.deposit_calculator
 import android.os.Bundle
 import android.text.Editable
 import android.text.InputType
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.mr_17.deposit_calculator.databinding.FragmentRecurringDepositBinding
 
 class RecurringDepositFragment : Fragment(R.layout.fragment_recurring_deposit) {
     private lateinit var binding: FragmentRecurringDepositBinding
+    private val database = Firebase.firestore
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -19,6 +24,17 @@ class RecurringDepositFragment : Fragment(R.layout.fragment_recurring_deposit) {
         (activity as MainActivity).setToolbarTitle(getString(R.string.recurring_deposit))
 
         initializeUI()
+
+        binding.apply {
+            btnCalculate.button.setOnClickListener{
+                calculate(
+                    getDays(
+                        etMonth.editText.text.toString().toInt(),
+                        etYear.editText.text.toString().toInt()
+                    )
+                )
+            }
+        }
     }
 
     private fun initializeUI() {
@@ -59,5 +75,31 @@ class RecurringDepositFragment : Fragment(R.layout.fragment_recurring_deposit) {
                 button.text = "Calculate"
             }
         }
+    }
+
+    private fun calculate(days: Int) {
+        /*Toast.makeText(context, days.toString(), Toast.LENGTH_SHORT).show()*/
+        database.collection("Interest")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    val interests = document.data.toSortedMap(compareBy<String> { it.length }.thenBy { it })
+                    var match = interests[interests.firstKey()]
+                    for (data in interests) {
+                        if(days < data.key.toInt()) {
+                            Toast.makeText(context, match.toString(), Toast.LENGTH_SHORT).show()
+                            break
+                        }
+                        match = data
+                    }
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("data", "Error getting documents.", exception)
+            }
+    }
+
+    private fun getDays(month: Int, year: Int): Int {
+        return (month * 30) + (year * 365)
     }
 }
